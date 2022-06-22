0dfb549eb0338f33f8dc5aa40e93a9f70974a115
Xayah06 <1185225218@qq.com>
2022-06-22 22:27:24 +0800

    Format

---
+++ user/fstest.c
@@ -0,0 +1,14 @@
+#include "lib.h"
+
+void umain()
+{
+	int fdnum;
+	char buf[512];
+	list_dir("/", buf);
+	writef("dir of / : %s\n", buf);
+	fdnum = open("/created_file", O_RDWR | O_CREAT);
+	close(fdnum);
+	list_dir("/", buf);
+	writef("dir of / : %s\n", buf);
+	while (1) ;
+}
